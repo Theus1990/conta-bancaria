c2f8f0618cce4ecaf0d2c5631ca8ace35a10528d
gabrielam0ura <gabrielamoura@alu.ufc.br>
2023-08-29 19:04:45 -0300

    Adicionando arquivos

--- conta.py
+++ conta.py
@@ -13,3 +13,10 @@ class Conta:
 
     def getLimite(self):
         return self.limite
+
+    def sacar(self, valor: float):
+        if valor>0 and valor<=self.saldo:
+            self.saldo-=valor
+            self.extrato.append('- Saldo')
+            return True
+        return False
